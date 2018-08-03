4d9524919474477eb3c9a092c420ba1ad6868df5
FuNing Xiang <654879744@qq.com>
2018-08-03 18:18:01 +0800

    initial project these are used git command as follow: 1.git clone *** 2.git status -> check the project file change status 3.git add    -> add new file or changed file to native repository 4.git commit -> commit temporary file to native repository 5.git config user.name&user.email ** -> configure commit info 6.git push   -> push update info to remote repository

---
+++ src/main/java/cn/yaobeier/morefun/MorefunApplication.java
@@ -0,0 +1,12 @@
+package cn.yaobeier.morefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorefunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorefunApplication.class, args);
+	}
+}
